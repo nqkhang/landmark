3292defd3dce7eb9be12e1918e4b3b46dc2cb4b1
Khang Q.NGUYEN <nguyenquockhang117@gmail.com>
2022-12-16 01:50:50 +0100

    add xcode project and WIP code

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Khang Nguyen on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
